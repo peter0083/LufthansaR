244676dd55d4be28e3cf10b82c88efa651bf3866
ptylin <peter.tingyao@alumni.ubc.ca>
2018-04-09 13:00:59 -0700

    moved unit test logics after the default value on departure.R and arrival.R

--- R/get_flight_status_arrival.R
+++ R/get_flight_status_arrival.R
@@ -15,6 +15,12 @@ get_flight_status_arrival <- function(airport = "FRA", fromDateTime = NULL){
   # operations/flightstatus/arrivals/{airportCode}/{fromDateTime}
   url_flightstatus_api <- "https://api.lufthansa.com/v1/operations/flightstatus/arrivals/"
 
+  # if no fromDateTime is specified, using Sys.time
+  if(is.null(fromDateTime)){
+    tm <- as.POSIXlt(Sys.time(), "", "%Y-%m-%dT%H:%M")
+    fromDateTime <-strftime(tm,  "%Y-%m-%dT%H:%M")
+  }
+
   # check fromDateTime class. The class should be "characer".
   if(class(fromDateTime) != "character"){
     stop("Problem with the fromDateTime input : ", fromDateTime, "-- Input CLASS should be 'character'. ie. '2018-04-09 12:27:11 PDT'")
@@ -23,12 +29,6 @@ get_flight_status_arrival <- function(airport = "FRA", fromDateTime = NULL){
   # check fromDateTime length. The length should be "1".
   if(length(fromDateTime) != 1){
     stop("Problem with the fromDateTime input : ", fromDateTime, "-- Input LENGTH should be '1'. ie. '2018-04-09 12:27:11 PDT'")
-  }
-
-  # if no fromDateTime is specified, using Sys.time
-  if(is.null(fromDateTime)){
-    tm <- as.POSIXlt(Sys.time(), "", "%Y-%m-%dT%H:%M")
-    fromDateTime <-strftime(tm,  "%Y-%m-%dT%H:%M")
   }
 
   url_flightstatus_api_airport <- paste0(url_flightstatus_api,airport,"/", fromDateTime)
