ef19fb1a0709a8e85876fc5d4c47176451d35d41
Amy Goldlist <amygoldlist@gmail.com>
2018-04-14 10:27:40 -0700

    refixed error

--- tests/testthat/test_functions.R
+++ tests/testthat/test_functions.R
@@ -5,9 +5,9 @@ cache_setting <- getOption("lufthansar_cache_token", default = NULL)
 options(lufthansar_token_cache = TRUE)
 testtoken <- get_token('../../.lufthansa-token')
 
-#####################
+#########################
 #### Functionality Tests
-####################
+#########################
 
 testthat::context("Basic functionality tests")
 
@@ -21,15 +21,6 @@ test_that('get_flight_status basic functionality', {
 
 })
 
-
-
-
-test_that('cget_flight_status_departure basic functionality', {
-  #outputs
-
-  expect_is(get_flight_status_departure(),'list')
-  expect_is(get_flight_status_departure(airport = "YVR"),'list')
-})
 
 
 test_that('airport basic functionality', {
@@ -69,8 +60,6 @@ testthat::test_that("airport throws errors correctly", {
 })
 
 
-skip('skip')
-
 testthat::test_that("get_flight_status_departure throws errors correctly", {
 
   testthat::expect_error(get_flight_status_departure(airport = "123"))
@@ -79,6 +68,8 @@ testthat::test_that("get_flight_status_departure throws errors correctly", {
 })
 
 
+skip('skip')
+
 test_that('get_flight_status_arrival basic functionality', {
   #outputs
 
@@ -86,6 +77,12 @@ test_that('get_flight_status_arrival basic functionality', {
   expect_is(get_flight_status_arrival(airport = "YYZ"),'list')
 })
 
-# Cleanup
-#file.remove('.other-name-token')
+
+test_that('get_flight_status_departure basic functionality', {
+  #outputs
+
+  expect_is(get_flight_status_departure(),'list')
+  expect_is(get_flight_status_departure(airport = "YVR"),'list')
+})
+
 
